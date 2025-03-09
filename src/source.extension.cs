a3cba2ded8915236e9b0668afe9bce76ae94a376
Mads Kristensen <madsk@microsoft.com>
2025-03-09 11:51:48 -0700

    Update extension name to Voice Typing
    Changed the `Name` constant in the `Vsix` class from "Speak" to "Voice Typing" for better clarity on the extension's functionality related to voice input.
    [release]

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -8,7 +8,7 @@ namespace Speak
     internal sealed partial class Vsix
     {
         public const string Id = "Speak.899ea22a-c6e2-43f6-b3eb-50a3713c4df7";
-        public const string Name = "Speak";
+        public const string Name = "Voice Typing";
         public const string Description = @"Hold down the CTRL key to speak to Visual Studio. Makes it easy to prompt Copilot or write code comments using your voice";
         public const string Language = "en-US";
         public const string Version = "1.0";
