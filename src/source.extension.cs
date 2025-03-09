c54c9b9290d8356508ab707bcebb0ca821320a58
Mads Kristensen <madsk@microsoft.com>
2025-03-08 22:38:17 -0800

    Update Vsix description for clarity and relevance
    Modified the `Description` constant in the `Vsix` class within `source.extension.cs`. The new description focuses on using voice commands to prompt Copilot or write code comments, removing references to markdown and the GitHub link for a clearer message.

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,8 +9,7 @@ namespace Speak
     {
         public const string Id = "Speak.899ea22a-c6e2-43f6-b3eb-50a3713c4df7";
         public const string Name = "Speak";
-        public const string Description = @"Hold down the CTRL key to speak to Visual Studio. Makes it easy to write markdown, code comments and prompt Copilot using your voiceHold down the 
-        https://github.com/madskristensen/Speak";
+        public const string Description = @"Hold down the CTRL key to speak to Visual Studio. Makes it easy to prompt Copilot or write code comments using your voice";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Mads Kristensen";
