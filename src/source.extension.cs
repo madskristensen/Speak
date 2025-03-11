c504962c850fc69862e8a6e259540568094033cb
Mads Kristensen <madsk@microsoft.com>
2025-03-11 10:16:02 -0700

    Now uses Ctrl+Shift to activate listening [release]

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,7 +9,7 @@ namespace Speak
     {
         public const string Id = "Speak.899ea22a-c6e2-43f6-b3eb-50a3713c4df7";
         public const string Name = "Voice Typing";
-        public const string Description = @"Hold down the CTRL key to speak to Visual Studio. Makes it easy to prompt Copilot or write code comments using your voice";
+        public const string Description = @"Hold down the CTRL+Shift keys to speak to Visual Studio. Makes it easy to prompt Copilot or write code comments using your voice";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Mads Kristensen";
